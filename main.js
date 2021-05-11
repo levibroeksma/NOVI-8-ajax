444b386c26757aa4a4643b00db077661c38cf50a
School <School@Levis-oude-bakbeest.local>
2021-05-11 18:04:47 +0200

    continued assignment, not yet completely finished

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-const axios = require('axios');
+// const axios = require('axios');
 
 async function getJoke() {
-    const result = await axios.get('https://api.chucknorris.io/jokes/random');
-    console.log(result.data.value);
+    try {
+        const result = await axios.get('https://api.chucknorris.io/jokes/random');
+        jokeContainer.textContent = result.data.value;
+    } catch (e) {
+        jokeContainer.textContent = "helaas pindakaas"
+    }
 }
 
+const jokeContainer = document.getElementById('chuckNorrisJoke');
+
 getJoke();
+
+// async function getCategories() {
+//     try {
+//         const result = await axios.get('https://api.chucknorris.io/jokes/categories')
+//         for (let i = 0; i < result.data.length; i++) {
+//             console.log(result.data[i]);
+//         }
+//     } catch (e) {
+//         console.log('too bad');
+//     }
+// }
+//
+// getCategories();
